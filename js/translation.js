b90d77a950ff47aa528f4b04eb85809f8ccda6ed
pictuga <pictuga@laptop-paul>
2011-06-11 17:10:19 +0200

    fixed translations (forgot to do bzr add), moved to stable, translated home readme to french

---
+++ js/translation.js
@@ -0,0 +1,40 @@
+function loadTranslation()
+{
+	var tr = new loader();
+	tr.url = "translation.txt";
+	tr.type = "ajax";
+	tr.callback = function(array)
+	{
+		var lang = array.xhr.getResponseHeader('Content-location').split('.').slice(-1)[0];
+		
+		if(lang == "en")
+		{
+			//
+		}
+		else
+		{
+			//need to get .en in case of missing translations, since .en will be done first
+		}
+		
+		parseTranslation(array.data);
+	}
+	tr.start();
+}
+
+function parseTranslation(input)
+{
+	input = input.split('\n').slice(0, -1);
+	for(var i in input)
+	{
+		var line = input[i].split('=', 2);
+		TRANSLATION[line[0]] = line[1].replace(/\\n/g, "\n").replace(/\\t/g, "\t");
+		//↑ dirty, but works
+	}
+}
+
+var TRANSLATION = {};
+
+function _(key)
+{
+	return TRANSLATION[key];
+}
